8ec20c84f0df675ed0ffcc7232ab074ae5b4c7f8
liramkreiner <154261070+liramkreiner@users.noreply.github.com>
2024-01-03 16:29:24 +0200

    Update lowercase.java

--- lowercase.java
+++ lowercase.java
@@ -1,5 +1,5 @@
 /** String processing exercise 1. */
-public class owerCase {
+public class lowerCase {
     public static void main(String[] args) {  
         String str = args[0];
         System.out.println(lowerCase(str)); //printing the new string after lowe hiw letter
